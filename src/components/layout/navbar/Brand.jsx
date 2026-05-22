10135c57b903af97016a2befb15a54c8a9d5f859
Daksh Chaudhary <Daksh.Chaudhary@Geminisolutions.com>
2026-05-22 09:47:21 +0530

    fix the navbar brand sizing

--- src/components/layout/navbar/Brand.jsx
+++ src/components/layout/navbar/Brand.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Brand = () => {
   return (
     <div className="flex items-center gap-2">
-      <Link to="/" className="text-3xl font-semibold tracking-tight sm:text-base">
+      <Link to="/" className="text-md font-semibold tracking-tight sm:text-md">
         Daksh Chaudhary
       </Link>
     </div>
